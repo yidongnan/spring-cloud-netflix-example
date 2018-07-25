9e8bf3be0244f221d7bee7b96ec7451c1960ba7b
Michael <yidongnan@gmail.com>
2018-07-25 23:05:15 +0800

    Spring Cloud Finchley example

--- service-a/src/main/java/net/devh/A1ServiceApplication.java
+++ service-a/src/main/java/net/devh/A1ServiceApplication.java
@@ -4,8 +4,8 @@ package net.devh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.annotations.Api;
--- service-b/src/main/java/net/devh/ServiceB1Controller.java
+++ service-b/src/main/java/net/devh/ServiceB1Controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,14 +23,13 @@ import io.swagger.annotations.Api;
 public class ServiceB1Controller {
 
     @Autowired
-    DiscoveryClient discoveryClient;
+    private Registration registration;
 
     @Value("${msg:unknown}")
     private String msg;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String printServiceB() {
-        ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
-        return serviceInstance.getServiceId() + " (" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + ")" + "===>Say " + msg;
+        return registration.getServiceId() + " (" + registration.getHost() + ":" + registration.getPort() + ")" + "===>Say " + msg;
     }
 }
